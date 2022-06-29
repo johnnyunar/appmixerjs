b59420676e345b08731f95fc846d748f61a9967e
Jan Unar <johnunar@gmail.com>
2022-06-29 09:43:59 +0200

    Simplified impossible button

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ function generateRectangleAreas() {
     const exercise1Div = document.getElementById("exercise1");
     exercise1Div.innerHTML = "";
     for (let i = 0; i < input; i++) {
-        exercise1Div.innerHTML += `<h2>${i}x${2 * i} rectangle area = ${getRectangleArea(i, 2 * i)}</h2>`;
+        exercise1Div.innerHTML += `<h2 class="h2">${i}x${2 * i} rectangle area = ${getRectangleArea(i, 2 * i)}</h2>`;
     }
 }
 
 const button = document.getElementById("impossibleButton");
-button.addEventListener("mouseenter", function( event ) {
-  event.target.style.color = "purple";
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
+button.addEventListener("mouseenter", function (event) {
+    let marginLeft = Number(event.target.style.marginLeft.replace("px", "")) + 300;
+    event.target.style.marginLeft = marginLeft + "px";
+});
+
 
