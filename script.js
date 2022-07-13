b54274ce4d859ffa5b6f1cfee650c9c5d2ace53a
Jan Unar <johnunar@gmail.com>
2022-07-13 11:14:35 +0200

    Refactored long code

--- script.js
+++ script.js
@@ -17,4 +17,115 @@ button.addEventListener("mouseenter", function (event) {
     event.target.style.marginLeft = marginLeft + "px";
 });
 
+function setCatHTMLAttributes(name, hunger, tiredness, loneliness) {
+    const catNameElement = document.getElementById("catName");
+    const catHungerElement = document.getElementById("catHunger");
+    const catTirednessElement = document.getElementById("catTiredness");
+    const catLonelinessElement = document.getElementById("catLoneliness");
+
+    catNameElement.innerHTML = "Name: " + name;
+    catHungerElement.innerHTML = "Hunger: " + hunger + "%";
+    catTirednessElement.innerHTML = "Tiredness: " + tiredness + "%";
+    catLonelinessElement.innerHTML = "Loneliness: " + loneliness + "%";
+}
+
+function checkNumInRange(num, min, max) {
+    if (num < min) {
+        num = min;
+    } else if (num > max) {
+        num = max;
+    }
+
+    return num;
+}
+
+function checkNumBetweenZeroHundred(num) {
+    return checkNumInRange(num, 0, 100);
+}
+
+function checkNumsInRange(numbers, min, max) {
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i] = checkNumInRange(numbers[i], min, max);
+    }
+    return numbers;
+}
+
+class Cat {
+    name;
+    hunger;
+    tiredness;
+    loneliness;
+
+    constructor(name) {
+        this.name = name;
+        this.hunger = 100;
+        this.tiredness = 100;
+        this.loneliness = 100;
+    }
+
+    meow() {
+        alert("MEOW!");
+    }
+
+    checkProperties() {
+        [this.hunger, this.tiredness, this.loneliness] = checkNumsInRange(
+            [this.hunger, this.tiredness, this.loneliness], 0, 100
+        );
+    }
+
+    feed() {
+        this.hunger -= 10;
+        this.loneliness -= 5;
+        this.tiredness += 5;
+        this.checkProperties();
+    }
+
+    pet() {
+        let wantsTo = Math.random() < 0.5; // 50% chance
+        if (wantsTo) {
+            this.loneliness -= 10;
+        } else {
+            this.loneliness -= 5;
+            this.tiredness += 20;
+        }
+        this.checkProperties();
+    }
+
+    sleep() {
+        this.tiredness -= 10;
+        this.loneliness += 5;
+        this.hunger += 10;
+        this.checkProperties();
+    }
+}
+
+let myCat = new Cat("Minda");
+
+setCatHTMLAttributes(myCat.name, myCat.hunger, myCat.tiredness, myCat.loneliness);
+
+const meowButton = document.getElementById("meow");
+meowButton.addEventListener("click", function () {
+    myCat.meow();
+});
+
+const feedButton = document.getElementById("feedButton");
+feedButton.addEventListener("click", function () {
+    myCat.feed();
+    setCatHTMLAttributes(myCat.name, myCat.hunger, myCat.tiredness, myCat.loneliness);
+});
+
+const petButton = document.getElementById("petButton");
+petButton.addEventListener("click", function () {
+    myCat.pet();
+    setCatHTMLAttributes(myCat.name, myCat.hunger, myCat.tiredness, myCat.loneliness);
+});
+
+const sleepButton = document.getElementById("sleepButton");
+sleepButton.addEventListener("click", function () {
+    myCat.sleep();
+    setCatHTMLAttributes(myCat.name, myCat.hunger, myCat.tiredness, myCat.loneliness);
+});
+
+
+
 
